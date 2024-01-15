2e59040d6178444ce00a658a53cddb399906f366
giovaniavila <giovanicarvalhoavila@hotmail.com>
2024-01-15 20:48:57 -0300

    style: adiciona scrollbar e altera cor do botao

--- todo/client/src/App.jsx
+++ todo/client/src/App.jsx
@@ -78,52 +78,70 @@ function App() {
   useEffect(() => {showTodos()}, [])
 
   return (
-    <div className='container-avo'>
-     <div className='container-pai'>
-       <h1 className='titulo'><img src={dataside} alt="" /> TODO </h1>
-       <section className='container'>
-        <div className='form'>
-          <form onSubmit={editMode ? editTodo : addToDo}>
-            <div className='form-wrapper' style={{display: "flex", justifyContent:"space-between"}}>
-              <div style={{flex: 1, marginRight:"10px"}}>
-                <input  onChange={(e) => setTask(e.target.value)} value={task} className='input' type='text' placeholder='Tasks' name='task' />
-              </div>
-              {
-                editMode ?
-                  <button type="submit"  className='btn'>Editar</button>
-                  :
-                  <button type="submit"  className='btn '>Adicionar task</button>
-              }
-            </div>
-          </form>
-        </div>
-
-        <table className='table'>
-          <div>
-            <thead className='elementable'>
-                <th scope='col'>Ordem</th>
-                <th scope='col'>Task</th>
-                <th scope='col'>Actions</th>
-            </thead>
-          </div>
-          <tbody>
-          {
-            Array.isArray(list) && list.map(d => (
-            <tr key={d.id} className='element-body'>
-              <th scope='row'>{d.id}</th>
-              <td>{d.task}</td>
-              <td className='icons'>
-                <i onClick={() => {showSingleToDo(d.id)}} className='fa-solid fa-pen-to-square' style={{color: '#5277C8', cursor: 'pointer'}}></i>
-                <i onClick={() => {deleteToDo(d.id)}} style={{color: "red", cursor:"pointer"}} className='fa-solid fa-trash-can'></i>
-              </td>
-            </tr>
-            ))
+    <div className='container-avo' style={{ overflowY: list.length > 9 ? 'scroll' : 'hidden' }}>
+      <style>
+        {`
+          .container-avo::-webkit-scrollbar {
+            width: 7px;
           }
-          </tbody>
-        </table>
 
-       </section>
-     </div>
+          .container-avo::-webkit-scrollbar-thumb {
+            background: linear-gradient(to right, #5277C8, #2E4B8E);
+            border-radius: 15px;
+          }
+
+          .container-avo::-webkit-scrollbar-track {
+            background-color: transparent;
+          }
+        `}
+      </style>
+      <div className='container-pai'>
+        <h1 className='titulo'><img src={dataside} alt="" /> TODO </h1>
+        <section className='container'>
+          <div className='form'>
+            <form onSubmit={editMode ? editTodo : addToDo}>
+              <div className='form-wrapper' style={{ display: "flex", justifyContent: "space-between" }}>
+                <div style={{ flex: 1, marginRight: "10px" }}>
+                  <input onChange={(e) => setTask(e.target.value)} value={task} className='input' type='text' placeholder='Tarefas' name='task' />
+                </div>
+                {
+                  editMode ?
+                    <button type="submit" className='btn'>Editar</button>
+                    :
+                    <button type="submit" className='btn '>Adicionar tarefa</button>
+                }
+              </div>
+            </form>
+          </div>
+
+          <div className='table-container'>
+            <table className='table'>
+              <div>
+                <thead className='elementable'>
+                  <th scope='col'>Ordem</th>
+                  <th scope='col'>Tarefa</th>
+                  <th scope='col'>Ações</th>
+                </thead>
+              </div>
+              <tbody>
+                {
+                  Array.isArray(list) && list.map(d => (
+                    <tr key={d.id} className='element-body'>
+                      <th scope='row'>{d.id}</th>
+                      <td>{d.task}</td>
+                      <td className='icons'>
+                        <i onClick={() => { showSingleToDo(d.id) }} className='fa-solid fa-pen-to-square' style={{ color: '#5277C8', cursor: 'pointer' }}></i>
+                        <i onClick={() => { deleteToDo(d.id) }} style={{ color: "red", cursor: "pointer" }} className='fa-solid fa-trash-can'></i>
+                      </td>
+                    </tr>
+                  ))
+                }
+              </tbody>
+            </table>
+          </div>
+
+        </section>
+      </div>
     </div>
   )
 }
